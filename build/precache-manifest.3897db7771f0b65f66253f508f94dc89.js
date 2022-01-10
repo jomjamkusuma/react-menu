5e8aa31b6629890f35ebbc73ea6e9d2dd9ba14d9
jomjamkusuma <67530519+jomjamkusuma@users.noreply.github.com>
2022-01-10 18:37:42 +0700

    upload file

---
+++ build/precache-manifest.3897db7771f0b65f66253f508f94dc89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "765243d575c6cdca2c5537ff575ab59d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d873c7e84c338d970b4e",
+    "url": "/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d873c7e84c338d970b4e",
+    "url": "/static/js/main.b65581eb.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
